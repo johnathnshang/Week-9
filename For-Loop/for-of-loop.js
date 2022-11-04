70c57e678e24917a399964b0e499c9a951476ade
Johnathan Shang <jshang5@se14l0120-11.ad.bcit.ca>
2022-11-04 15:17:52 -0700

    upload

---
+++ For-Loop/for-of-loop.js
@@ -0,0 +1,11 @@
+var weathers = ["sunny", "rainy", "cloudy"];
+var words = "";
+
+for(var weather of weathers){
+    words += weather;{
+    break;
+}
+words += weather + "\n";
+}
+
+console.log(words);
